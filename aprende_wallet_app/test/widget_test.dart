eeafb6689ef75aa1359853cd39700126ddeda009
KGP2005 <20221044@aloe.ulima.edu.pe>
2025-12-02 00:53:41 -0500

    ..

--- aprende_wallet_app/test/widget_test.dart
+++ aprende_wallet_app/test/widget_test.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:aprende_wallet_app/pages/Planificacion/pagos_planificados/pagos_planificados_page.dart';
